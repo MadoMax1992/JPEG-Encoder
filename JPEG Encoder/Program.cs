a8d43d5b834b7ff896855b5aeb41c3b2891b5a2d
Max Blücher <max13041@yahoo.de>
2018-10-16 17:45:51 +0200

    initial

--- JPEG Encoder/Program.cs
+++ JPEG Encoder/Program.cs
@@ -10,12 +10,49 @@ namespace JPEG_Encoder
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            //2-D-Datenstruktur ?
+            List<RGBPixel> RGBImage = new List<RGBPixel>();
+            RGBPixel data = new RGBPixel();
+
+            // Example
+
+            //data.greenValue = 5;
+            //data.blueValue = 5;
+            //data.redValue = 5;
+            //data.xPixelCord = 1;
+            //data.yPixelCord = 2;
+
+            //Data.Add(data);
+
+
+
+            string line;
+
+
+            // Öffnet ein Stream, liest die Datei ein und gibt Zeilenweise die rgb pixel aus
+
+            try
+            {
+                System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\Max-FH\Documents\Visual Studio 2017\Projects\JPEG Encoder\Bilder\red-blue-light.ppm");
+                while ((line = file.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(line);
+
+                }
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                //Aussagekräftige meldung
+            }
+
+
+            // lässt die console offen  
+            System.Console.ReadLine();
+
         }
     }
 }
---
+++ JPEG Encoder/RGBPixel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEG_Encoder
+{
+    class RGBPixel
+    {
+        public int xPixelCord { get; set; }
+        public int yPixelCord { get; set; }
+        public byte greenValue { get; set; }
+        public byte redValue { get; set; }
+        public byte blueValue { get; set; }
+    }
+}
